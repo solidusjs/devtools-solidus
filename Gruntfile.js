ea9e72919c2915ea42ee5f6ec334743c0c1fa5f2
Josiah Sprague <josiah.sprague@gmail.com>
2014-06-26 20:39:06 -0400

    Switch to Grunt for managing dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['*.js', 'views/*.js'],
+      options: {
+        jshintrc: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
